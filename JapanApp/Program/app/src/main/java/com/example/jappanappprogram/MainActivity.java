26d8dd7fc1f875d0d1002f905e23a50ea252b68a
Mateusz <bloxorzykigi@gmail.com>
2020-04-03 18:40:35 +0200

    Dodane Intenty do menu głównego i wybory systemu znakowego

--- JapanApp/Program/app/src/main/java/com/example/jappanappprogram/MainActivity.java
+++ JapanApp/Program/app/src/main/java/com/example/jappanappprogram/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.jappanappprogram;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -21,7 +22,25 @@ public class MainActivity extends AppCompatActivity {
         btDraw.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent drawIntent= new Intent(MainActivity.this,com.example.jappanappprogram.SystemChoiceActivity.class);
+                drawIntent.putExtra("mode","draw");
+                startActivity(drawIntent);
+            }
+        });
+        btGuess.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent guessIntent = new Intent(MainActivity.this, com.example.jappanappprogram.SystemChoiceActivity.class);
+                guessIntent.putExtra("mode", "guess");
+                startActivity(guessIntent);
 
+            }
+        });
+        btCredits.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent creditsIntent = new Intent(MainActivity.this, com.example.jappanappprogram.CreditsActivity.class);
+                startActivity(creditsIntent);
             }
         });
     }
--- JapanApp/Program/app/src/main/java/com/example/jappanappprogram/SystemChoiceActivity.java
+++ JapanApp/Program/app/src/main/java/com/example/jappanappprogram/SystemChoiceActivity.java
@@ -2,7 +2,9 @@ package com.example.jappanappprogram;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class SystemChoiceActivity extends AppCompatActivity {
@@ -15,6 +17,54 @@ public class SystemChoiceActivity extends AppCompatActivity {
         btKatakana=findViewById(R.id.btKatakana);
         btKanji=findViewById(R.id.btKanji);
 
+        btHiragana.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String mode= getIntent().getStringExtra("mode");
+                if (mode.equals("draw")){
+                    Intent hiragana= new Intent(SystemChoiceActivity.this, com.example.jappanappprogram.DrawItActivity.class);
+                    hiragana.putExtra("system","hiragana");
+                    startActivity(hiragana);
+                }
+                else if(mode.equals("guess")){
+                    Intent hiragana = new Intent( SystemChoiceActivity.this, com.example.jappanappprogram.GuessItActivity.class);
+                    hiragana.putExtra("system","hiragana");
+                    startActivity(hiragana);
+                }
+            }
+        });
 
+        btKatakana.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String mode= getIntent().getStringExtra("mode");
+                if (mode.equals("draw")){
+                    Intent katakana= new Intent(SystemChoiceActivity.this, com.example.jappanappprogram.DrawItActivity.class);
+                    katakana.putExtra("system","katakana");
+                    startActivity(katakana);
+                }
+                else if(mode.equals("guess")){
+                    Intent katakana = new Intent( SystemChoiceActivity.this, com.example.jappanappprogram.GuessItActivity.class);
+                    katakana.putExtra("system","katakana");
+                    startActivity(katakana);
+                }
+            }
+        });
+        btKanji.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String mode= getIntent().getStringExtra("mode");
+                if (mode.equals("draw")){
+                    Intent kanji= new Intent(SystemChoiceActivity.this, com.example.jappanappprogram.DrawItActivity.class);
+                    kanji.putExtra("system","kanji");
+                    startActivity(kanji);
+                }
+                else if(mode.equals("guess")){
+                    Intent kanji = new Intent( SystemChoiceActivity.this, com.example.jappanappprogram.GuessItActivity.class);
+                    kanji.putExtra("system","kanji");
+                    startActivity(kanji);
+                }
+            }
+        });
     }
 }
