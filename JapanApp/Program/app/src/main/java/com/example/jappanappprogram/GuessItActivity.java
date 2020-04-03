3a5312713add7c2da426ad24e72edf597fa7d49c
Mateusz <bloxorzykigi@gmail.com>
2020-04-03 18:12:34 +0200

    Pozostałe Activity

---
+++ JapanApp/Program/app/src/main/java/com/example/jappanappprogram/GuessItActivity.java
@@ -0,0 +1,14 @@
+package com.example.jappanappprogram;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GuessItActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guess_it);
+    }
+}
