93a386f18ac5e19283aa02ce0aa26a63466ce71a
Mateusz <bloxorzykigi@gmail.com>
2020-04-10 01:51:26 +0200

    Wstępny ekran do rysowania palcem

--- JapanApp/Program/app/src/main/java/com/example/jappanappprogram/DrawItActivity.java
+++ JapanApp/Program/app/src/main/java/com/example/jappanappprogram/DrawItActivity.java
@@ -3,12 +3,19 @@ package com.example.jappanappprogram;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 
 public class DrawItActivity extends AppCompatActivity {
+
+    private PaintView paintView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_draw_it);
+        paintView = (PaintView) findViewById(R.id.paintView);
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        paintView.init(metrics);
     }
 }
