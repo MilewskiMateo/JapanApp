6f55e143166226b06d8003cf2aa04b0ff64cf313
Mateusz <bloxorzykigi@gmail.com>
2020-04-13 04:30:21 +0200

    Poprawki kolorów

--- JapanApp/Program/app/src/main/java/com/example/jappanappprogram/GuessItActivity.java
+++ JapanApp/Program/app/src/main/java/com/example/jappanappprogram/GuessItActivity.java
@@ -2,13 +2,21 @@ package com.example.jappanappprogram;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class GuessItActivity extends AppCompatActivity {
+    ImageView imgSign;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guess_it);
+
+        imgSign = findViewById(R.id.imgSign);
+        imgSign.setImageResource(R.drawable.ba);
+        imgSign.getLayoutParams().height = 500;
+        imgSign.getLayoutParams().width = 500;
     }
 }
