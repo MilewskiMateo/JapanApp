0f6812fca5fef76dd42722e44550bde514dea06d
Mateusz <bloxorzykigi@gmail.com>
2020-04-17 21:31:31 +0200

    Udane pierwsze zaimplementowanie bazy danych przy pomocą android room. LiveData działa dobrze.

--- JapanApp/Program/app/src/main/java/com/example/jappanappprogram/GuessItActivity.java
+++ JapanApp/Program/app/src/main/java/com/example/jappanappprogram/GuessItActivity.java
@@ -1,6 +1,10 @@
 package com.example.jappanappprogram;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.drawable.Drawable;
@@ -12,10 +16,11 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import java.io.File;
+import java.util.List;
 
 public class GuessItActivity extends AppCompatActivity {
     ImageView imgSign;
-    Button answer1;
+    Button answer1,answer4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,14 +28,29 @@ public class GuessItActivity extends AppCompatActivity {
         setContentView(R.layout.activity_guess_it);
 
         answer1= findViewById(R.id.btAnswer1);
+        answer4= findViewById(R.id.btAnswer4);
         imgSign = findViewById(R.id.imgSign);
         imgSign.setImageResource(R.drawable.ba);
         imgSign.getLayoutParams().height = 500;
         imgSign.getLayoutParams().width = 500;
 
+        final WordViewModel mWordViewModel = ViewModelProviders.of(this).get(WordViewModel.class);
+        mWordViewModel.getAllWords().observe(this, new Observer<List<Word>>() {
+            @Override
+            public void onChanged(@Nullable final List<Word> words) {
+                answer4.setText(words.get(words.size()-1).getWord());
+
+
+            }
+        });
+
         answer1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Word slowo = new Word("slowo");
+                mWordViewModel.insert(slowo);
+
+
             }
         });
 
--- JapanApp/Program/app/src/main/java/com/example/jappanappprogram/MainActivity.java
+++ JapanApp/Program/app/src/main/java/com/example/jappanappprogram/MainActivity.java
@@ -1,19 +1,56 @@
 package com.example.jappanappprogram;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
     Button btDraw,btGuess,btCredits;
+
+    public static final int NEW_WORD_ACTIVITY_REQUEST_CODE = 1;
+
+    private WordViewModel mWordViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
+
+        // Get a new or existing ViewModel from the ViewModelProvider.
+        final WordViewModel mWordViewModel = ViewModelProviders.of(this).get(WordViewModel.class);
+        mWordViewModel.getAllWords().observe(this, new Observer<List<Word>>() {
+            @Override
+            public void onChanged(@Nullable final List<Word> words) {
+                // Update the cached copy of the words in the adapter.
+                
+
+            }
+        });
+
+
+
+
+
+        // Add an observer on the LiveData returned by getAlphabetizedWords.
+        // The onChanged() method fires when the observed data changes and the activity is
+        // in the foreground.
+
+
+
+
 
         btDraw=findViewById(R.id.btDraw);
         btGuess=findViewById(R.id.btGuess);
