324ff14a5906dfa996e62b9e54e573d7972f32f8
Mateusz <bloxorzykigi@gmail.com>
2020-05-04 20:19:07 +0200

    zapisywanie rysunku

--- JapanApp/Program/app/src/main/java/com/example/jappanappprogram/DrawItActivity.java
+++ JapanApp/Program/app/src/main/java/com/example/jappanappprogram/DrawItActivity.java
@@ -2,20 +2,78 @@ package com.example.jappanappprogram;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class DrawItActivity extends AppCompatActivity {
 
+    private TextView tvSign, tvDrawIt;
+    private Button btDone;
     private PaintView paintView;
+    String actualWord;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_draw_it);
+
         paintView = (PaintView) findViewById(R.id.paintView);
         DisplayMetrics metrics = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(metrics);
         paintView.init(metrics);
+
+        tvSign=findViewById(R.id.tvSign);
+        tvDrawIt=findViewById(R.id.tvDrawIt);
+        btDone=findViewById(R.id.btDone);
+
+        String signSystem = getIntent().getStringExtra("system");
+
+        switch (signSystem) {
+            case "hiragana":
+                actualWord = RandomWordPicker.getOneRandomHiraKataWord();
+                tvDrawIt.setText(R.string.hiragana );
+                tvSign.setText(actualWord);
+                break;
+            case "katakana":
+                actualWord = RandomWordPicker.getOneRandomHiraKataWord();
+                tvDrawIt.setText(R.string.katakana);
+                tvSign.setText(actualWord);
+                break;
+            case "kanji":
+                actualWord = RandomWordPicker.getOneRandomKanjiWord();
+                tvDrawIt.setText(R.string.kanji);
+                tvSign.setText(actualWord);
+                break;
+        }
+
+
+        btDone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                paintView.clear();
+
+                switch (signSystem) {
+                    case "hiragana":
+                        actualWord = RandomWordPicker.getOneRandomHiraKataWord();
+                        tvSign.setText(actualWord);
+
+                        break;
+                    case "katakana":
+
+                        actualWord = RandomWordPicker.getOneRandomHiraKataWord();
+                        tvSign.setText(actualWord);
+                        break;
+                    case "kanji":
+
+                        actualWord = RandomWordPicker.getOneRandomKanjiWord();
+                        tvSign.setText(actualWord);
+                        break;
+                }
+            }
+        });
     }
 }
--- JapanApp/Program/app/src/main/java/com/example/jappanappprogram/PaintView.java
+++ JapanApp/Program/app/src/main/java/com/example/jappanappprogram/PaintView.java
@@ -1,6 +1,7 @@
 package com.example.jappanappprogram;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BlurMaskFilter;
 import android.graphics.Canvas;
@@ -9,12 +10,18 @@ import android.graphics.EmbossMaskFilter;
 import android.graphics.MaskFilter;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.PorterDuff;
+import android.os.Environment;
 import android.util.AttributeSet;
 import android.util.DisplayMetrics;
 import android.view.MotionEvent;
 import android.view.View;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
+
+import static android.content.Context.MODE_PRIVATE;
 
 
 public class PaintView extends View {
@@ -27,13 +34,10 @@ public class PaintView extends View {
     private Path mPath;
     private Paint mPaint;
     private ArrayList<FingerPath> paths = new ArrayList<>();
-    private int currentColor;
     private int backgroundColor = DEFAULT_BG_COLOR;
-    private int strokeWidth;
-    private MaskFilter mEmboss;
     private Bitmap mBitmap;
     private Canvas mCanvas;
-    private Paint mBitmapPaint = new Paint(Paint.DITHER_FLAG);
+
 
     public PaintView(Context context) {
         this(context, null);
@@ -41,6 +45,8 @@ public class PaintView extends View {
 
     public PaintView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        mPath = new Path();
+
         mPaint = new Paint();
         mPaint.setAntiAlias(true);
         mPaint.setDither(true);
@@ -50,8 +56,10 @@ public class PaintView extends View {
         mPaint.setStrokeCap(Paint.Cap.ROUND);
         mPaint.setXfermode(null);
         mPaint.setAlpha(0xff);
+        mPaint.setStrokeWidth(BRUSH_SIZE);
 
-        mEmboss = new EmbossMaskFilter(new float[] {1, 1, 1}, 0.4f, 6, 3.5f);
+
+
     }
 
     public void init(DisplayMetrics metrics) {
@@ -60,60 +68,77 @@ public class PaintView extends View {
 
         mBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
         mCanvas = new Canvas(mBitmap);
-
-        currentColor = DEFAULT_COLOR;
-        strokeWidth = BRUSH_SIZE;
-    }
-
-
-
-    public void clear() {
-        backgroundColor = DEFAULT_BG_COLOR;
-        paths.clear();
-        invalidate();
-    }
-
-    @Override
-    protected void onDraw(Canvas canvas) {
-        canvas.save();
         mCanvas.drawColor(backgroundColor);
 
-        for (FingerPath fp : paths) {
-            mPaint.setColor(fp.color);
-            mPaint.setStrokeWidth(fp.strokeWidth);
-            mPaint.setMaskFilter(mEmboss);
-            mCanvas.drawPath(fp.path, mPaint);
 
-        }
+    }
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
 
-        canvas.drawBitmap(mBitmap, 0, 0, mBitmapPaint);
-        canvas.restore();
+        canvas.drawBitmap(mBitmap, 0, 0, null);
     }
 
-    private void touchStart(float x, float y) {
-        mPath = new Path();
-        FingerPath fp = new FingerPath(currentColor, strokeWidth, mPath);
-        paths.add(fp);
 
-        mPath.reset();
+    private void touchStart(float x, float y) {
         mPath.moveTo(x, y);
         mX = x;
         mY = y;
+    }
+    public void clear() {
+        FileOutputStream fos = null;
+
+        try
+        {
+            fos = this.getContext().openFileOutput("narywowany", MODE_PRIVATE);
+            mBitmap.compress(Bitmap.CompressFormat.PNG, 100, fos);
+
+            fos.flush();
+            fos.close();
+            fos = null;
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            if (fos != null)
+            {
+                try
+                {
+                    fos.close();
+                    fos = null;
+                }
+                catch (IOException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+
+        System.out.println(Environment.getExternalStorageDirectory());
+
+
+        mCanvas.drawColor(backgroundColor);
+        invalidate();
     }
 
     private void touchMove(float x, float y) {
         float dx = Math.abs(x - mX);
         float dy = Math.abs(y - mY);
-
         if (dx >= TOUCH_TOLERANCE || dy >= TOUCH_TOLERANCE) {
-            mPath.quadTo(mX, mY, (x + mX) / 2, (y + mY) / 2);
+            mPath.quadTo(mX, mY, (x + mX)/2, (y + mY)/2);
             mX = x;
             mY = y;
+            mCanvas.drawPath(mPath, mPaint);
         }
     }
 
     private void touchUp() {
-        mPath.lineTo(mX, mY);
+
+        mPath.reset();
     }
 
     @Override
@@ -121,21 +146,26 @@ public class PaintView extends View {
         float x = event.getX();
         float y = event.getY();
 
-        switch(event.getAction()) {
-            case MotionEvent.ACTION_DOWN :
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
                 touchStart(x, y);
-                invalidate();
                 break;
-            case MotionEvent.ACTION_MOVE :
+            case MotionEvent.ACTION_MOVE:
                 touchMove(x, y);
                 invalidate();
                 break;
-            case MotionEvent.ACTION_UP :
+            case MotionEvent.ACTION_UP:
                 touchUp();
-                invalidate();
                 break;
+            default:
         }
-
         return true;
+    }
+
+    public static int getScreenWidth() {
+        return Resources.getSystem().getDisplayMetrics().widthPixels;
+    }
+    public static int getScreenHeight() {
+        return Resources.getSystem().getDisplayMetrics().heightPixels;
     }
 }
